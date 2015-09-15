f156ec85c9f8310d468fe8bd5ae047fb31ab16ce
Axel Orn <axel@absalon.is>
2015-09-15 16:02:57 +0000

    Add web server with live reload gulp task

--- README.md
+++ README.md
@@ -1,14 +1,15 @@
 # awesome-game (working title)
 
-A simple breakout game from the people that brought you absolutely nothing at
-all.
+A simple breakout game from the people that brought you absolutely nothing at all.
 
 ## Installation
 
 Prerequisites: npm
 
+```
 npm install
 npm install -g gulp
-gulp watch
+gulp
+```
 
-and serve up the directory on a web server and you should be good
+This will run the default gulp task which will build the application, start a web server and reload the page for each change inside the `./pub/` folder. 
--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,13 @@
 var gulp = require('gulp');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
+var connect = require('gulp-connect');
 
-gulp.task('default', ['build']);
+gulp.task('default', ['serve', 'watch']);
 
 gulp.task('watch', ['build'], function() {
   gulp.watch('app.js', ['build']);  
+  gulp.watch(['./pub/*.*'], ['reload']);
 });
 
 gulp.task('build', function() {
@@ -14,5 +16,17 @@ gulp.task('build', function() {
         //Pass desired output filename to vinyl-source-stream
         .pipe(source('bundle.js'))
         // Start piping stream to tasks!
-        .pipe(gulp.dest('./'));
+        .pipe(gulp.dest('./pub/'));
+});
+
+gulp.task('serve', function () {
+  connect.server({
+    root: 'pub',
+    livereload: true
+  });
+});
+
+gulp.task('reload', function () {
+  gulp.src('./pub/*.*')
+    .pipe(connect.reload());
 });
